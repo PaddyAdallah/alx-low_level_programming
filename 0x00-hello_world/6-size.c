ce9113a9eecde2dc8cf4586571fd261ba99669ae
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-03 02:02:46 +0300

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,9 +16,9 @@ int main(void)
 
 	/*Size of data types */
 	printf("Size of a char: %zu byte (s)\n", sizeof(charType));
-   	printf("Size of an int: %zu byte (s)\n", sizeof(intType));
-    	printf("Size of a long: %zu byte (s)\n", sizeof(longType));
-    	printf("Size of a long int: %zu byte (s)\n", sizeof(longintType));
+	printf("Size of an int: %zu byte (s)\n", sizeof(intType));
+	printf("Size of a long: %zu byte (s)\n", sizeof(longType));
+	printf("Size of a long int: %zu byte (s)\n", sizeof(longintType));
 	printf("Size of a float: %zu byte (s)\n", sizeof(floatType));
 	return (0);
 }
