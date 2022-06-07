6dfa0473535f7f6b30d1e4dda25dedd0dbd80697
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-07 05:27:03 +0300

    [200~Variables, if, else, while~

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,19 +13,19 @@ int main(void)
 
 	for (tens = 0; tens <= 9; tens++)
 	{
-		 for (ones = 0; ones <= 9; ones++)
+		for (ones = 0; ones <= 9; ones++)
 	{
-	       	putchar(tens + '0');
+		putchar(tens + '0');
 		putchar(ones + '0');
 
-	  if (!(tens == 9 && ones == 9))
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+	if (!(tens == 8 && ones == 9))
+	{
+		putchar(',');
+		putchar(' ');
 	}
-    }
-  putchar('\n');
+	}
+	}
+      	putchar('\n');
 
   return (0);
 }
