18134b9b0638c30a990aacab3588d3cac5481cbd
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-09 23:42:52 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,12 @@
 /**
 * _isupper - checks for uppercase letter
 * @c: variable
-*
 * Return: int 
 */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c<='Z')
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
