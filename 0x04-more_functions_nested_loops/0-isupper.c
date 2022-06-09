194f9acd62e9838d367e86387cf8467a33dae2dd
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-09 22:59:22 +0300

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int _isupper(int c)
+{
+        int c;
+
+	if (c == 'C')
+	{
+		return (1);
+	}else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(int character);
+int _isupper(int c);
+
+
+#endif /* MAIN_H */
+
