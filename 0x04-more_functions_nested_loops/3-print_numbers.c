9a076d98c61d483e6558d97d344004a0c232ec29
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-10 00:24:50 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers
- * Return
+* print_numbers - prints numbers from 0 to 9
+* Return: int
+*/
+
+void print_numbers(void)
+{
+int i;
+
+for (i >= 0; i <= 10; i++)
+{
+	_putchar(i);
+}
+_putchar(32);
+}
