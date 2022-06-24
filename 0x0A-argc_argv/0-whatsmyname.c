21d337f4dc36341c01a0d297ac0ffb84a7714710
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-24 23:11:21 +0300

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,13 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - program that prints its name
- * @argc: argument count
- * @argv: argunment vector
- *
- * Return: int
- */
-
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-	int count;
-
-	for (count = 0; count < argc; count++)
-	{
-		if (argc == 1)
-			printf("%s\n", argv[count]);
-		else
-			break;
-	}
+	argc = argc;
+	printf("%s\n", *argv);
 	return (0);
 }
